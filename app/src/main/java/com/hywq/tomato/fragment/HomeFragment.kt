9327890b3597ceb7b506281cca087f52a148a4ef
liuyuzhe <>
2021-10-22 14:13:39 +0800

    ndk c的代码
    Signed-off-by: liuyuzhe <>

--- app/src/main/java/com/hywq/tomato/fragment/HomeFragment.kt
+++ app/src/main/java/com/hywq/tomato/fragment/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.hywq.tomato.R
 import com.hywq.tomato.view.HttpsTrustManager
@@ -14,6 +15,10 @@ import java.security.SecureRandom
 import javax.net.ssl.*
 
 class HomeFragment : Fragment() {
+
+    init {
+        System.loadLibrary("ndk_yuwq")
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,8 +37,11 @@ class HomeFragment : Fragment() {
 
         HttpsTrustManager.allowAllSSL()
 
-        val webView = view.findViewById<WebView>(R.id.webView)
-//        webView.loadUrl("https://m.ymatou.com/consignment/myCoupon?winType=19&bgcolor=ffffff")
-        webView.loadUrl("https://m.ymatou.com/consignment/myCoupon?bgcolor=ffffff&UserId=26279592&AccessToken=0E1E1DE6CBB40F0D6F0E40B2E1877F9429F6FDBE1DE4581428455CE629E8C8A1F629465716CE192EBC22A5FD96744330F03B8FE86926DC70&DeviceId=ffffffff-d173-43c3-63a9-e1b41a6bf5df&DeviceToken=ffffffff-d173-43c3-63a9-e1b41a6bf5df&imei=867165046099191&winType=19&oaid=3f6ef1e7832baa5f")
+        val textJni = view.findViewById<TextView>(R.id.textJni)
+        textJni.text = stringFromJNI().orEmpty()
     }
+
+    external fun stringFromJNI(): String?
+
+
 }
